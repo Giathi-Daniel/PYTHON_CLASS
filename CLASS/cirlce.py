893de8d8a7c3a6e989d9b7b54b78839044089db6
Giathi Daniel <giathidaniel252@gmail.com>
2024-02-26 14:23:55 +0300

    Added: Updated

--- CLASS/cirlce.py
+++ CLASS/cirlce.py
@@ -1,2 +1,16 @@
 class Circle:
-    
+    def __init__(self, radius):
+        self.radius = radius
+        self.diameter = 2 * radius
+
+    def Area(self):
+        return 3.14 * self.radius * self.radius
+
+    def Perimeter(self):
+        return 3.14 * self.diameter
+
+cir = Circle(7)
+print(cir.radius)
+print(cir.diameter)
+print(cir.Area())
+print(cir.Perimeter())
