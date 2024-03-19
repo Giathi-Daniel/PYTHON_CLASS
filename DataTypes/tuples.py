93caed76baa2a849f451f55e4174a603b9d2742c
Daniel-Giathi <giathidaniel252@gmail.com>
2024-03-19 18:26:50 +0300

    tuples

---
+++ DataTypes/tuples.py
@@ -0,0 +1,14 @@
+mytuple = ("Javascript", "Python", "React", "React", 1, True) #store multiple items in a singlr variable
+print(type(mytuple))
+print(mytuple)
+print(len(mytuple))
+print(mytuple[3])
+print(mytuple)
+
+# loop the whole tuple
+for x in mytuple:
+    print(x)
+
+# # loop through index
+# for i in range(len(mytuple)):
+#     print(mytuple[i])
