ea703aeb40829a38256ed52630a42d3fe41f0f30
Daniel-Giathi <giathidaniel252@gmail.com>
2024-03-19 16:21:34 +0300

    Variables & DataTypes

---
+++ DataTypes/typeConversion.py
@@ -0,0 +1,22 @@
+x = 124
+y = -12.289
+z = 10j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Type conversion
+x = float(x)
+print(type(x))
+
+y = int(y)
+print(type(y))
+
+
+
+# Complex numbers cannot be converted to any type
+# y = complex(-12.289E3820832098302982)
+# print(type(y))
+
+
