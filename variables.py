35375785ef0faa11e1fa2baaf825aeba2fa7fa64
Daniel-Giathi <giathidaniel252@gmail.com>
2024-04-01 06:47:45 +0300

    variables

--- variables.py
+++ variables.py
@@ -31,3 +31,11 @@ def func1():
     return x
 func1()
 print(x)
+
+
+#Rules for naming a variable
+    # 1. Should start with a letter or underscore
+    # 2. Should not start with a number
+    # 3. Reserved keywords such as and cannot be used in naming variables
+    # 4. Variable names are key sensitive (age, Age are not the same)
+    # 5. Variable names only contain alpha-numeric characters and undescores
