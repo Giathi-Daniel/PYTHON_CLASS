3054d25e6bbe3570f91efc61a89f3fb07ad3d93e
Daniel-Giathi <giathidaniel252@gmail.com>
2024-04-04 00:08:51 +0300

    AreaofCircle

--- CLASS/cirlce.py
+++ CLASS/cirlce.py
@@ -3,14 +3,14 @@ class Circle:
         self.radius = radius
         self.diameter = 2 * radius
 
-    def area(self):
+    def getArea(self):
         return 3.14 * self.radius * self.radius
 
-    def perimeter(self):
+    def getCircumference(self):
         return 3.14 * self.diameter
 
 cir = Circle(7)
 print(cir.radius)
 print(cir.diameter)
-print(cir.area())
-print(cir.perimeter())
+print('Area::',cir.getArea())
+print("Circumference ::", cir.getCircumference())
