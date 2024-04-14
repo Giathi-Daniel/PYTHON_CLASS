50f10b634d19a5168d0ac628e5547f655496de91
Daniel-Giathi <giathidaniel252@gmail.com>
2024-04-14 12:46:21 +0300

    list and for

--- Tests/list.py
+++ Tests/list.py
@@ -3,3 +3,8 @@ listTwo = [20, 40, 60, 80]
 
 print(listOne == listTwo)
 print(listOne is listTwo)
+
+# Part two
+sampleList = ["Jon", "Kelly", "Jessa"]
+sampleList.append(2, "Scott")
+print(sampleList)
