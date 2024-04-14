104d1b1905717ce7c23d748483e522d9e892640c
Daniel-Giathi <giathidaniel252@gmail.com>
2024-04-14 12:52:45 +0300

    global & local variables

--- Tests/methods.py
+++ Tests/methods.py
@@ -3,3 +3,13 @@ def calculate (num1, num2=4):
   print(res)
 
 calculate(5, 6)
+
+# quiz two
+salary = 8000
+
+def printSalary():
+  salary = 12000
+  print("Salary:", salary)
+  
+printSalary()
+print("Salary:", salary)
