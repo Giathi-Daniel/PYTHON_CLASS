e9917eec00f30000907ad4ca1f2746788b8847fa
Daniel-Giathi <giathidaniel252@gmail.com>
2024-04-16 07:32:20 +0300

    If...elif functions

--- controlFlow/if2.py
+++ controlFlow/if2.py
@@ -4,3 +4,7 @@ if n%2 == 0:
     print("Number " + str(n) + " is even.")
 else:
     print("Number " + str(n) + " is odd.")
+
+# Python program to illustrate short hand if-else
+j = 30
+print("Number " + str(j) + " is even.") if n == 4 else print("Number " + str(j) + " is odd.")
