dbba61816e437f06a0b9b839beeabd5580920f7f
Daniel-Giathi <giathidaniel252@gmail.com>
2024-04-16 07:25:34 +0300

    PythonDictionary

--- DataTypes/sets.py
+++ DataTypes/sets.py
@@ -11,8 +11,22 @@ print(myset)
 myset.remove("banana")
 print(myset)
 
-myset.pop()
+myset.pop() #removes and returns an arbitrary set element. Raise KeyError if set is empty.
 print(myset)
 
 myset.clear()
 print(myset)
+
+#program to sum all indices in the set
+# set1 = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+# for i in range(len(set1)):
+#     result = i+ i
+#     print(result)
+
+#program to calcualte the sum of the els in the set
+set1 = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+total_sum = 0
+for item in set1:
+    total_sum += item
+    print(total_sum)
