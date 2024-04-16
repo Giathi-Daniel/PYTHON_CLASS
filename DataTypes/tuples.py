8b7b6f30138dddc2bd92636eb570499919d3f9b9
Daniel-Giathi <giathidaniel252@gmail.com>
2024-04-16 06:55:12 +0300

    Tuples

--- DataTypes/tuples.py
+++ DataTypes/tuples.py
@@ -1,8 +1,9 @@
-mytuple = ("Javascript", "Python", "React", "React", 1, True) #store multiple items in a single variable
+mytuple = (1, "Javascript", "Python", "React", "React", True) #store multiple items in a single variable
 print(type(mytuple)) #ordered, unchangleable, allow duplicates
 print(mytuple)
 print(len(mytuple))
 print(mytuple[3])
+print(mytuple[0])
 print(mytuple)
 
 # loop the whole tuple
